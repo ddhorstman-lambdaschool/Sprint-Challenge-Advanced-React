e4a2f6af10c965e66bf27e6d7846f648a36a60f4
ddhorstman <davidzilla27@gmail.com>
2020-03-22 22:11:07 -0400

    Wrote some tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from "@testing-library/react";
+import WorldCupContainer from './components/WorldCupContainer';
+import { act } from 'react-dom/test-utils';
+import useCountryFlag from './hooks/useCountryFlag';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test("contains a header", () => {
+  let container;
+  act(() => {
+    container = render(<WorldCupContainer />);
+  });
+  expect(container.getByText(/players/i));
+});
+
+test("can retrieve a valid country flag", () => {
+  const url = useCountryFlag("United States")[0];
+  expect(url).toBe("https://www.countryflags.io/US/flat/64.png");
+});
+
+test("throws an error for bad country names", () => {
+  try {
+    const url = useCountryFlag("Bluenited States")[0];
+    //intentionally fail the test if the previous line doesn't throw an error
+    expect(url).toContain("bananananananananana");    
+  }
+  catch(e){
+    //look for the error message originating from the hook
+    expect(e).toContain("not a valid country name");
+  }
 });
